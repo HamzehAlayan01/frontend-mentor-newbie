1732b94d6fe79e49ad5d2d3532cabbc6cd03cc81
Hamzeh Alayan <hamzehalayan01@gmail.com>
2025-03-05 14:01:10 +0300

    article preview initial commit

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,19 @@
+let shareBtn = document.querySelector('.share-img');
+let shareLinks = document.querySelector('.share-box');
+
+shareBtn.addEventListener('mouseenter', () => {
+    shareLinks.style.display = 'flex';
+});
+
+// shareBtn.addEventListener('mouseleave', () => {
+//     shareLinks.style.display = 'none';
+// });
+shareLinks.addEventListener('mouseenter', () => {
+    shareLinks.style.display = 'flex';
+});
+
+shareLinks.addEventListener('mouseleave', () => {
+    shareLinks.style.display = 'none';
+});
+
+
